3d17799fece192fb15cd61de15f43ad6e7b5089c
LiXiang <xianglqqo@gmail.com>
2019-06-12 14:19:05 +0800

    将驼峰join命名改为下划线

--- test/find/where.js
+++ test/find/where.js
@@ -7,11 +7,11 @@ const { Ormv, model } = require('../db/');
 const { $sql, $in, $as } = Ormv.Op;
 const { tasks } = model;
 
-test('leftJoin', async t => {
+test('left_join', async t => {
 
    const result = await tasks
       .select('tasks.id', 'keywords', $as("tasks.email", "xx"))
-      .leftJoin("user")
+      .left_join("user")
       .on({ 'tasks.id': 'user.id' })
       .where({
          'tasks.id': $in(50, 51),
@@ -39,7 +39,7 @@ test('leftJoin', async t => {
 })
 
 
-test('rightJoin', async t => {
+test('right_join', async t => {
 
    const result = await tasks
       .select('tasks.id', 'keywords', $as("tasks.email", "xx"))
